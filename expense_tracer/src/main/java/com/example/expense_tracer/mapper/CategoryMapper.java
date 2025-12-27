515a408c0310ceb3afba4dd2152ef2c048b2756a
Zbuny <zhan.ospanov@narxoz.kz>
2025-12-27 17:51:12 +0500

    fix

--- expense_tracer/src/main/java/com/example/expense_tracer/mapper/CategoryMapper.java
+++ expense_tracer/src/main/java/com/example/expense_tracer/mapper/CategoryMapper.java
@@ -3,11 +3,15 @@ package com.example.expense_tracer.mapper;
 import com.example.expense_tracer.dto.CategoryDto;
 import com.example.expense_tracer.entity.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
+    @Mapping(source = "user.id", target = "userId")
     CategoryDto toDto(Category category);
+    @Mapping(source = "userId", target = "user.id")
     Category toEntity(CategoryDto dto);
     List<CategoryDto> toDtoList(List<Category> categories);
 }
